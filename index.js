c793d7a2e7c0b9037cb0307bad489bdfc5ea316a
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-08-23 10:45:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = "funkyFunction"
 
-function funkyFunction() {
-  return theFunk(); {
+function theFunk() {
     return "FUNKY!"
-  }
 }
 
